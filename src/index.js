f70e80434d0e522dd9757013239616df544dfb6e
Sviatlana Kuraptsova <kuraptsova.s@gmail.com>
2018-02-22 22:22:12 +0300

    added solution: to be optimized later

--- src/index.js
+++ src/index.js
@@ -2,4 +2,46 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-}
+    let num = currency;
+    let coins = [50, 25, 10, 5, 1];
+    let exchange = [];
+    coins.forEach(function (v) {
+        if (num % v === 0) {
+            exchange.push(num / v);
+            num = 0;
+        }
+        else {
+            exchange.push(Math.floor(num / v));
+            num = num - v * (Math.floor(num / v));
+        }
+
+    });
+    let result = {},
+        half = "H",
+        quarter = "Q",
+        dime = "D",
+        nickel = "N",
+        penny = "P",
+
+        err = {},
+        message = "error";
+    err[message] = "You are rich, my friend! We don\'t have so much coins for exchange";
+
+    let keys = [half, quarter, dime, nickel, penny];
+    exchange.forEach(function (v, i) {
+            if (v > 0) {
+                result[keys[i]] = v;
+            }
+            else {
+                return result
+            }
+        }
+    );
+    if (currency >= 10000) {
+        return err;
+    }
+    else {
+
+        return result;
+    }
+};
